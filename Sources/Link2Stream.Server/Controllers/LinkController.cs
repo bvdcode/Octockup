2702e0a0d02b87fcd7bdae99aa6026670a52cf56
Vadim Belov <github@belov.us>
2024-11-21 23:08:18 -0800

    Initial commit

---
+++ Sources/Link2Stream.Server/Controllers/LinkController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Link2Stream.Server.Controllers
+{
+    [ApiController]
+    public class LinkController : ControllerBase
+    {
+        [HttpGet("/{linkId}")]
+        public IActionResult GetLink([FromRoute][Required] string linkId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("/link")]
+        public IActionResult CreateLink([FromBody][Required] string url)
+        {
+            return Ok();
+        }
+    }
+}
