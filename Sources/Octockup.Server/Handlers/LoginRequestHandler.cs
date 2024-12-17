1fc2dc1270f973125840d1980ff98d39b00de87c
Vadim Belov <github@belov.us>
2024-12-17 06:32:03 -0800

    Refactor LoginRequestHandler and update JWT settings
    Removed WebApiException class. Updated LoginRequestHandler with new dependencies and refactored Handle method for user validation, logging, token creation, and session management. Added GetUserClaims method. Updated appsettings.Development.json with new JWT settings.

--- Sources/Octockup.Server/Handlers/LoginRequestHandler.cs
+++ Sources/Octockup.Server/Handlers/LoginRequestHandler.cs
@@ -1,28 +1,53 @@
-﻿using EasyExtensions.AspNetCore.Authorization.Services;
-using MediatR;
-using Octockup.Server.Database;
+﻿using MediatR;
+using AutoMapper;
 using Octockup.Server.Models;
+using System.Security.Claims;
+using Octockup.Server.Database;
+using Octockup.Server.Models.Dto;
+using EasyExtensions.EntityFrameworkCore.Exceptions;
+using EasyExtensions.AspNetCore.Authorization.Services;
+using EasyExtensions.AspNetCore.Authorization.Builders;
 
 namespace Octockup.Server.Handlers
 {
-    public class LoginRequestHandler(ILogger<LoginRequestHandler> _logger, ITokenProvider _tokenProvider) : IRequestHandler<LoginRequest, AuthResponse>
+    public class LoginRequestHandler(ILogger<LoginRequestHandler> _logger, ITokenProvider _tokenProvider,
+        AppDbContext _dbContext, IConfiguration _configuration, IMapper _mapper) : IRequestHandler<LoginRequest, AuthResponse>
     {
-        public Task<AuthResponse> Handle(LoginRequest request, CancellationToken cancellationToken)
+        public async Task<AuthResponse> Handle(LoginRequest request, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("Login attempt for '{Username}'", request.Username);
-            string token = _tokenProvider.CreateToken(x =>
+            var foundUser = _dbContext.Users.FirstOrDefault(x => x.Username.Equals(request.Username, StringComparison.CurrentCultureIgnoreCase)) 
+                ?? throw new WebApiException(System.Net.HttpStatusCode.NotFound, nameof(User), "User not found");
+            if (!foundUser.PasswordHash.Equals(request.PasswordHash))
             {
-                return x.Add(ClaimTypes.Name, request.Username)
-                        .Add(ClaimTypes.Sid, "1");
-            });
+                _logger.LogWarning("Login attempt for '{Username}' failed", request.Username);
+                throw new WebApiException(System.Net.HttpStatusCode.Unauthorized, nameof(User), "Invalid password");
+            }
+            _logger.LogInformation("User '{Username}' logged in", request.Username);
+            string accessToken = _tokenProvider.CreateToken(x => GetUserClaims(x, foundUser));
+            int refreshLifetime = _configuration.GetValue<int>("JwtSettings:RefreshLifetimeHours");
+            if (refreshLifetime <= 0)
+            {
+                throw new InvalidOperationException("JwtSettings:RefreshLifetimeHours must be greater than 0");
+            }
+            TimeSpan refreshLifetimeSpan = TimeSpan.FromHours(refreshLifetime);
             Session session = new()
             {
-                UserId = 1,
-                RefreshToken = _tokenProvider.CreateToken(x => x.Add(ClaimTypes.Name, "refresh"))
+                User = foundUser,
+                UserId = foundUser.Id,
+                RefreshToken = _tokenProvider.CreateToken(refreshLifetimeSpan, x => GetUserClaims(x, foundUser)),
             };
             _dbContext.Sessions.Add(session);
-            await _dbContext.SaveChangesAsync();
-            return Ok(new AuthResponse(token, session.RefreshToken));
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            UserDto userDto = _mapper.Map<UserDto>(foundUser);
+            return new(accessToken, session.RefreshToken, userDto);
+        }
+
+        private static ClaimBuilder GetUserClaims(ClaimBuilder builder, User user)
+        {
+            return builder.Add(ClaimTypes.Name, user.Username)
+                    .Add(ClaimTypes.Role, user.Role.ToString())
+                    .Add(ClaimTypes.Email, user.Email)
+                    .Add(ClaimTypes.Sid, user.Id.ToString());
         }
     }
 }
