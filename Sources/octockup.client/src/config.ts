8f5fc381e23d396f999f12d7af48cfa846c1a585
Vadim Belov <github@belov.us>
2024-12-17 06:54:31 -0800

    Update API_BASE_URL to use local backend conditionally based on URL

--- Sources/octockup.client/src/config.ts
+++ Sources/octockup.client/src/config.ts
@@ -1,2 +1,6 @@
-export const API_BASE_URL = "http://localhost:5112/api/v1";
+export const API_BASE_URL =
+  // if current url contains "localhost", use the local backend
+  window.location.href.includes("localhost")
+    ? "http://localhost:5112/api/v1"
+    : "/api/v1";
 export const LOCAL_STORAGE_REFRESH_TOKEN_KEY = "OCTOCKUP_REFRESH_TOKEN";
