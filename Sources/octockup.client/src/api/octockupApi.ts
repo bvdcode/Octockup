c855554c99d795f1cb023091f1b7f9aef434a29e
Vadim Belov <github@belov.us>
2024-12-16 17:58:51 -0800

    Refactor login function to use structured request and response interfaces

--- Sources/octockup.client/src/api/octockupApi.ts
+++ Sources/octockup.client/src/api/octockupApi.ts
@@ -1,17 +1,33 @@
 import SHA512 from "crypto-js/sha512";
 
-export const login = async (username: string, password: string) => {
-  const passwordHash = SHA512(password).toString();
+interface LoginRequest {
+  username: string;
+  passwordHash: string;
+}
+
+interface LoginResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export const login = async (
+  username: string,
+  password: string
+): Promise<LoginResponse> => {
+  const request = {
+    username: username,
+    passwordHash: SHA512(password).toString(),
+  } as LoginRequest;
   const response = await fetch("http://localhost:5112/api/v1/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username, passwordHash }),
+    body: JSON.stringify(request),
   });
 
   if (response.ok) {
-    const data = await response.json();
+    const data: LoginResponse = await response.json();
     return data;
   } else {
     throw new Error("Login failed");
