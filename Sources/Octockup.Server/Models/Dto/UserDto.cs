db809f215b2db34d0f5fd92e84f61e294c3dcc50
Vadim Belov <github@belov.us>
2024-12-17 06:33:17 -0800

    Add Email and Role properties to UserDto
    Updated namespace declaration to include Octockup.Server.Database.Enums.
    Added Email and Role properties to UserDto, with Role being of type UserRole.

--- Sources/Octockup.Server/Models/Dto/UserDto.cs
+++ Sources/Octockup.Server/Models/Dto/UserDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Octockup.Server.Models.Dto
+﻿using Octockup.Server.Database.Enums;
+
+namespace Octockup.Server.Models.Dto
 {
-    public record UserDto(int Id, string Username);
+    public record UserDto(int Id, string Username, string Email, UserRole Role);
 }
--- Sources/octockup.client/src/api/types.ts
+++ Sources/octockup.client/src/api/types.ts
@@ -12,4 +12,12 @@ export interface AuthResponse {
 export interface User {
   id: number;
   username: string;
+  email: string;
+  role: UserRole;
+}
+
+export enum UserRole {
+  Blocked = 0,
+  Regular = 1,
+  Admin = 2,
 }
