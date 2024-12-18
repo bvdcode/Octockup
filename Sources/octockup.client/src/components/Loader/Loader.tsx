ce10045aaec85b5713cc3cb91d2dad9937660fb2
Vadim Belov <github@belov.us>
2024-12-18 15:25:13 -0800

    Add Loader component and localization support for loading messages

---
+++ Sources/octockup.client/src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import { useTranslation } from "react-i18next";
+
+interface LoaderProps {
+  text: string;
+}
+
+const Loader: React.FC<LoaderProps> = ({ text }) => {
+  const { t } = useTranslation();
+  return <>{text ?? t("loader.loading")}</>;
+};
+
+export default Loader;
