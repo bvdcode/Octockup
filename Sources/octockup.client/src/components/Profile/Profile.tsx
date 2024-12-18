a2770a371f1565682743e2c45d55389c51f1039e
Vadim Belov <github@belov.us>
2024-12-18 15:03:35 -0800

    Refactor Profile component to enhance UI with Material-UI and improve layout

--- Sources/octockup.client/src/components/Profile/Profile.tsx
+++ Sources/octockup.client/src/components/Profile/Profile.tsx
@@ -1,27 +1,47 @@
-import styles from "./Profile.module.css";
 import AxiosClient from "../../api/AxiosClient";
-import { Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const Profile: React.FC = () => {
   return (
-    <div className={styles.profileContainer}>
-      <h1>Profile</h1>
-      <div className={styles.changePasswordContainer}>
-        <TextField
-          className={styles.passwordField}
-          label="New Password"
-          type="password"
-          variant="outlined"
-          color="primary"
-        />
-      </div>
-      <Button
-        className={styles.logoutButton}
-        onClick={() => AxiosClient.events.emit("logout")}
-      >
-        Logout
-      </Button>
-    </div>
+    <Box sx={{ p: 2 }}>
+      <Typography variant="h1" component="h1">
+        Profile
+      </Typography>
+      <Card sx={{ my: 2, p: 2 }}>
+        <CardContent>
+          <TextField
+            label="New Password"
+            type="password"
+            variant="outlined"
+            color="primary"
+            fullWidth
+            sx={{ mb: 2 }}
+          />
+          <Button variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+            Change Password
+          </Button>
+        </CardContent>
+      </Card>
+      <Card sx={{ my: 2, p: 2 }}>
+        <CardContent>
+          <Button
+            variant="contained"
+            color="secondary"
+            fullWidth
+            onClick={() => AxiosClient.events.emit("logout")}
+          >
+            Logout
+          </Button>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
