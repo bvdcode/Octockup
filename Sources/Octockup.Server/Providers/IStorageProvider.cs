87a35474aea916ee6048ab863b92a227e9fe79ec
Vadim Belov <github@belov.us>
2024-12-18 22:05:33 -0800

    Add Name and Parameters to IStorageProvider interface
    The IStorageProvider interface now requires implementing classes to provide a Name property and a Parameters property. The ScpProvider class has been updated to include these new properties, but their implementations currently throw NotImplementedException.

--- Sources/Octockup.Server/Providers/IStorageProvider.cs
+++ Sources/Octockup.Server/Providers/IStorageProvider.cs
@@ -2,6 +2,8 @@
 {
     public interface IStorageProvider
     {
+        string Name { get; }
+        IEnumerable<string> Parameters { get; }
         IEnumerable<RemoteFileInfo> GetAllFiles();
     }
 }
--- Sources/Octockup.Server/Providers/ScpProvider.cs
+++ Sources/Octockup.Server/Providers/ScpProvider.cs
@@ -2,6 +2,10 @@
 {
     public class ScpProvider : IStorageProvider
     {
+        public string Name => throw new NotImplementedException();
+
+        public IEnumerable<string> Parameters => throw new NotImplementedException();
+
         public IEnumerable<RemoteFileInfo> GetAllFiles()
         {
             throw new NotImplementedException();
