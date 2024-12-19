1da6393e5b94361a22cb5e19be3800c873345a4b
Vadim Belov <github@belov.us>
2024-12-18 19:51:07 -0800

    Refactor NavBar styles and structure for improved hover functionality and layout

--- Sources/octockup.client/src/components/NavBar/NavBar.tsx
+++ Sources/octockup.client/src/components/NavBar/NavBar.tsx
@@ -1,10 +1,11 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Box, Button } from "@mui/material";
 import styles from "./NavBar.module.css";
-import { useNavigate } from "react-router-dom";
 
 interface NavButton {
   path: string;
-  icon: JSX.Element;
+  icon: React.ReactNode;
 }
 
 interface NavBarProps {
@@ -15,21 +16,18 @@ const NavBar: React.FC<NavBarProps> = (props) => {
   const navigate = useNavigate();
 
   return (
-    <Box className={styles.navbar}>
-      {props.buttons.map((button, index) => (
-        <Button
-          key={index}
-          sx={{
-            justifyContent: "center",
-            borderRadius: "50%",
-            width: "64px",
-            height: "64px",
-          }}
-          onClick={() => navigate(button.path)}
-        >
-          {button.icon}
-        </Button>
-      ))}
+    <Box className={styles.navbarContainer}>
+      <Box className={styles.navbar}>
+        {props.buttons.map((button, index) => (
+          <Button
+            key={index}
+            sx={{ justifyContent: "center" }}
+            onClick={() => navigate(button.path)}
+          >
+            {button.icon}
+          </Button>
+        ))}
+      </Box>
     </Box>
   );
 };
