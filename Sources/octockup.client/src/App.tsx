97a3f7f6d9605634923269461cd7cbdd54ec156b
Vadim Belov <github@belov.us>
2024-12-19 20:48:27 -0800

    Refactor App component to use memoized theme creation; improve theme state management

--- Sources/octockup.client/src/App.tsx
+++ Sources/octockup.client/src/App.tsx
@@ -1,26 +1,28 @@
-import { User } from "./api/types";
-import RoutesComponent from "./Routes";
-import { useSystemTheme } from "./theme";
-import AuthProvider from "react-auth-kit";
-import { useEffect, useState } from "react";
-import { refresh } from "./auth/AuthKitMethods";
 import { ToastContainer } from "react-toastify";
 import CssBaseline from "@mui/material/CssBaseline";
 import createStore from "react-auth-kit/createStore";
-import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useEffect, useMemo, useState } from "react";
+import { useSystemTheme } from "./theme";
+import AuthProvider from "react-auth-kit";
+import RoutesComponent from "./Routes";
+import { refresh } from "./auth/AuthKitMethods";
+import { User } from "./api/types";
 
 function App() {
   const systemTheme = useSystemTheme();
-  const [theme, setTheme] = useState(systemTheme);
+  const [theme, setTheme] = useState(createTheme(systemTheme));
   const store = createStore<User>({
     authName: "_octockup_auth",
     authType: "localstorage",
     refresh: refresh,
   });
 
+  const memoizedTheme = useMemo(() => createTheme(systemTheme), [systemTheme]);
+
   useEffect(() => {
-    setTheme(systemTheme);
-  }, [systemTheme]);
+    setTheme(memoizedTheme);
+  }, [memoizedTheme]);
 
   return (
     <ThemeProvider theme={theme}>
