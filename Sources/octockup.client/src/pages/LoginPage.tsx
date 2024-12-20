26c4d4834e2a5b56e936147e7dfc5f04c947d7fe
Vadim Belov <github@belov.us>
2024-12-19 19:00:13 -0800

    Add loading state to LoginPage and enhance Loader component with progress indicator

--- Sources/octockup.client/src/pages/LoginPage.tsx
+++ Sources/octockup.client/src/pages/LoginPage.tsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import { login } from "../api/api";
 import { toast } from "react-toastify";
 import { LoginForm } from "../components";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import useSignIn from "react-auth-kit/hooks/useSignIn";
+import Loader from "../components/Loader";
 
 const LoginPage: React.FC = () => {
   const signIn = useSignIn();
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const { t } = useTranslation();
 
   const onLogin = (email: string, password: string) => {
+    setIsLoading(true);
     login(email, password)
       .then((response) => {
         const signedIn = signIn({
@@ -30,9 +34,17 @@ const LoginPage: React.FC = () => {
       })
       .catch((error) => {
         toast.error(t("login.error", { error: error.message }));
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
-  return <LoginForm onLogin={onLogin} />;
+  return (
+    <>
+      {isLoading && <Loader />}
+      <LoginForm onLogin={onLogin} />
+    </>
+  );
 };
 
 export default LoginPage;
