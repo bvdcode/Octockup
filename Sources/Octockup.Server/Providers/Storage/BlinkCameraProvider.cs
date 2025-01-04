a0c71017a4f119e1d32239adb932743f4129e22b
Vadim Belov <github@belov.us>
2025-01-04 01:49:08 -0800

    Add BlinkCameraProvider class implementing IStorageProvider
    Introduced BlinkCameraProvider in Octockup.Server.Providers.Storage
    namespace. This class implements the IStorageProvider<BaseStorageParameters>
    interface, including a Parameters property with a default value, a Name
    property returning "Blink Camera Storage", and a GetAllFiles method
    returning an empty list. Added a using directive for Octockup.Server.Models.

---
+++ Sources/Octockup.Server/Providers/Storage/BlinkCameraProvider.cs
@@ -0,0 +1,16 @@
+﻿using Octockup.Server.Models;
+
+namespace Octockup.Server.Providers.Storage
+{
+    public class BlinkCameraProvider : IStorageProvider<BaseStorageParameters>
+    {
+        public BaseStorageParameters Parameters { get; set; } = null!;
+
+        public string Name => "Blink Camera Storage";
+
+        public IEnumerable<RemoteFileInfo> GetAllFiles()
+        {
+            return [];
+        }
+    }
+}
