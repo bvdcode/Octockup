f76e0554695c88ba53a088fe356f9ef4817deade
Vadim Belov <github@belov.us>
2025-01-09 21:04:07 -0800

    Add is_deleted column to backup_snapshots table for soft deletion

---
+++ Sources/Octockup.Server/Migrations/PostgresMigrations/20250110050358_AddTaskFlags.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Octockup.Server.Migrations.PostgresMigrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskFlags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "backup_snapshots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "backup_snapshots");
+        }
+    }
+}
