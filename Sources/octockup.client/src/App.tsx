84bf9eb7f492c1fdeaaac72807162fa161954b91
Vadim Belov <bvdcode@users.noreply.github.com>
2025-11-23 14:11:23 -0800

    feat: add Material UI icons and update App component with new routes

--- Sources/octockup.client/src/App.tsx
+++ Sources/octockup.client/src/App.tsx
@@ -1,4 +1,11 @@
 import { AppShell } from "@bvdcode/react-kit";
+import {
+  Backup,
+  CloudDone,
+  CloudDownload,
+  Home,
+  Schedule,
+} from "@mui/icons-material";
 
 function App() {
   return (
@@ -7,8 +14,34 @@ function App() {
       logoUrl="/octockup.png"
       pages={[
         {
+          icon: <Home />,
+          name: "Home",
           route: "/",
           component: <div>Home Page</div>,
+        },
+        {
+          icon: <Backup />,
+          name: "Sources",
+          route: "/sources",
+          component: <div>Sources Page</div>,
+        },
+        {
+          icon: <CloudDownload />,
+          name: "Storages",
+          route: "/storages",
+          component: <div>Storages Page</div>,
+        },
+        {
+          icon: <CloudDone />,
+          name: "Tasks",
+          route: "/tasks",
+          component: <div>Tasks Page</div>,
+        },
+        {
+          icon: <Schedule />,
+          name: "Schedules",
+          route: "/schedules",
+          component: <div>Schedules Page</div>,
         },
       ]}
     />
