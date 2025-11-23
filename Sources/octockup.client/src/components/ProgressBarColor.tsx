e1f16b293e926ae0332680965e52fde24376f448
Vadim Belov <bvdcode@users.noreply.github.com>
2025-11-23 13:27:07 -0800

    Refactor project to Vite + React setup
    - Updated `Dockerfile` and `Octockup.sln` to reflect the new path for `octockup.client.esproj`.
    - Removed `eslint.config.js`, `index.html`, and `octockup.client.esproj` files, indicating a shift in project architecture.
    - Added new assets: `octockup.png`, `vite.svg`, and `react.svg`.
    - Updated `App.css`, `index.css`, and `main.tsx` to define styles and initialize the React app.
    - Simplified `App.tsx` to include a counter and links to Vite and React documentation.
    - Updated `package.json` and `package-lock.json` to streamline dependencies for React, TypeScript, and Vite.
    - Removed legacy files, including localization, theming, and unused components, reflecting a major project restructuring.

--- Sources/octockup.client/src/components/ProgressBarColor.tsx
+++
@@ -1,6 +0,0 @@
-export enum ProgressBarColor {
-  Neutral = "#999999aa",
-  Red = "#ff657caa",
-  Green = "#5aff74aa",
-  Yellow = "#feff66aa",
-}
--- Sources/octockup.client/src/config.ts
+++
@@ -1,7 +0,0 @@
-export const API_BASE_URL =
-  // if current url contains "localhost", use the local backend
-  window.location.href.includes("localhost")
-    ? "http://localhost:5112/api/v1"
-    : "/api/v1";
-export const LOCAL_STORAGE_LANGUAGE_KEY = "_octockup_language";
-export const MIN_PASSWORD_LENGTH = 6;
