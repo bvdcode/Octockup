a92a6f0b30b18d08de4a1a55df856df75bbab4a2
Vadim Belov <bvdcode@users.noreply.github.com>
2025-11-23 16:57:48 -0800

    feat: add BackupStorageController to manage backup storage retrieval

---
+++ Sources/Octockup.Server/Controllers/BackupStorageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Octockup.Server.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Octockup.Server.Controllers
+{
+    [ApiController]
+    public class BackupStorageController(IEnumerable<IBackupStorage> _backupStorages) : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("/api/v1/backups/storages")]
+        public IActionResult GetBackupStorages()
+        {
+            var mapped = _backupStorages.Select(x => new
+            {
+                name = x.Name,
+                id = x.GetType().FullName,
+                parameters = x.RequiredParameters,
+            });
+            return Ok(mapped);
+        }
+    }
+}
