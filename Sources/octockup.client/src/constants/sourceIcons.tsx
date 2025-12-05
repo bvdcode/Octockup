c84c2cbde79055cb672c6743307925ea0ab16857
Vadim Belov <bvdcode@users.noreply.github.com>
2025-12-04 22:52:01 -0800

    Add IMAP source icon to sourceIcons mapping

--- Sources/octockup.client/src/constants/sourceIcons.tsx
+++ Sources/octockup.client/src/constants/sourceIcons.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Avatar } from "@mui/material";
-import { Folder, HelpOutline } from "@mui/icons-material";
+import { Email, Folder, HelpOutline } from "@mui/icons-material";
 
 // Mapping of backend source IDs to icons. Extend manually as needed.
 const ICONS: Record<string, React.ReactNode> = {
+  "Octockup.Server.Modules.IMAPSource": <Email />,
   "Octockup.Server.Modules.FileSystemBackupSource": <Folder />,
-  "Octockup.Server.Modules.S3BackupStorage": <Avatar src="/assets/s3.svg" sx={{ p: 1 }} />,
-  "Octockup.Server.Modules.SFTPBackupStorage": <Avatar src="/assets/sftp.png" sx={{ p: 1 }} />,
+  "Octockup.Server.Modules.S3BackupStorage": (
+    <Avatar src="/assets/s3.svg" sx={{ p: 1 }} />
+  ),
+  "Octockup.Server.Modules.SFTPBackupStorage": (
+    <Avatar src="/assets/sftp.png" sx={{ p: 1 }} />
+  ),
 };
 
 export function getSourceIcon(id: string): React.ReactNode {
