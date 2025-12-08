0c379eaf63721b8cbe95902836108286a7b21822
Vadim Belov <git@belov.us>
2025-12-08 15:20:24 -0800

    Add license headers and update using directives in controllers
    Added SPDX AGPL-3.0-only license and 2025 copyright headers to BackupController.cs, ScheduleController.cs, and SnapshotController.cs. Reorganized and expanded using directives to include necessary namespaces for each controller.

--- src/Octockup.Server/Controllers/SnapshotController.cs
+++ src/Octockup.Server/Controllers/SnapshotController.cs
@@ -1,4 +1,7 @@
-﻿using Mapster;
+﻿// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (c) 2025 Vadim Belov
+
+using Mapster;
 using MimeKit;
 using Octockup.Server.Streams;
 using Microsoft.AspNetCore.Mvc;
